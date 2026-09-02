db487ca0595ef99cbaac5b1e4127ba01661b7f2f
TentelcomdelOeste <tentelcom.info@gmail.com>
2026-09-02 02:44:21 -0600

    fix(camera): use native capture pipeline

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
-import './services/instantCapture';
 import App from './App.tsx';
 import './index.css';
 
